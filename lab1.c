6781f00143fb73bab4b57486fb9d890076641c85
rhltray <75441694+rhltray@users.noreply.github.com>
2023-12-14 01:42:48 -0500

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -2,4 +2,44 @@
  * Implement your solution in thi file
  *
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tgmath.h>
+#include "lab1.h"
 
+char* readString(char* fileName){
+    FILE *fp = fopen(fileName, "r");
+    char *buffer = calloc(MAX_LINE_LEN, sizeof(char));
+    fgets(buffer, MAX_LINE_LEN, fp);
+    // printf("%s\n", buffer);
+    buffer[strcspn(buffer, "\n")] = 0;
+    printf("%s\n", buffer);
+    fclose(fp);
+
+    return buffer;
+}
+int summ(const char *str)
+{
+    int n = strlen(str);
+    int size = (pow(n, 2) + n) / 2;
+    return size;
+}
+char *mysteryExplode(const char *str)
+{
+    int size = summ(str);
+
+    char *buffer = calloc(size, sizeof(char));
+    char *temp = calloc(size, sizeof(char));
+
+    for (int i = 0; i < strlen(str); i++){
+        for (int j = 0; j < i + 1; j++) {
+            temp[j] = str[j];
+        }
+        strcat(buffer, temp);
+    }
+
+    free(temp);
+
+    return buffer;
+}
